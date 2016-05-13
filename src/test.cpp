047a2664c031b9ec9cbe67bc8c880057c513035c
Vladislav <valeinik00@gmail.com>
2016-05-13 22:34:32 +0300

    New mostly c-style representation

--- src/test.cpp
+++ src/test.cpp
@@ -1,17 +1,22 @@
 #include "headers/MyException.hpp"
-#include <csdtlib>
+#include <cstdlib>
 #include <iostream>
 
+using namespace MyExceptionCharStringRepresentation;
 
 int main()
 {
 	try
 	{
-		throw MyException("Get rect", __FILE__, "int main()", __LINE__);
+		throw Exception("Unfixable bug", __FILE__, __PRETTY_FUNCTION__, __LINE__, 
+			  Exception(     "Kind off", __FILE__, __PRETTY_FUNCTION__, __LINE__,
+			  Exception(     "No jokes", __FILE__, __PRETTY_FUNCTION__, __LINE__,
+			  Exception(          "...", __FILE__, __PRETTY_FUNCTION__, __LINE__,
+			  Exception(          "Neh", __FILE__, __PRETTY_FUNCTION__, __LINE__)))));
 	}
 	catch (const std::exception& ex)
 	{
-		std::cout << rx.what() << std::endl;
+		std::cout << ex.what() << std::endl;
 	}
 	catch (...)
 	{
